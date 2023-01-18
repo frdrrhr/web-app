f587074dbae0f384b362674223b4a71b5750dce5
frdrrhr <40151057+frdrrhr@users.noreply.github.com>
2023-01-18 21:07:20 +0100

    Create guess.php

---
+++ week-6-guessing-game/guess.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Guessing Game for Friedrich Döhrer</title>
+</head>
+<body>
+<h1>Welcome to my guessing game</h1>
+<p>Your guess is too low</p>
+</body>
+</html>
